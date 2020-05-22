c17e5e088d2a4454f537ced7e56cd3f03e7a0b2b
jessicaguo <jsg88@nau.edu>
2020-05-22 16:03:08 -0600

    checking the depths of SOM

--- initial_analyses/simple_plots_acrosspools.R
+++ initial_analyses/simple_plots_acrosspools.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ae6a7fe2387b4bc9d079c30739b759f58bc12e05ed7ba232efcee83770c21d9
-size 7891
+oid sha256:dd4720027201bb0d117f07155153a84a51d999ae2608cf67cd20096e065bd2a0
+size 8208
