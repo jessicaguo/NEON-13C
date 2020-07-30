26c9efabcb8f3a2a57a0dddcc3d5031010a942e9
jessicaguo <jsg88@nau.edu>
2020-07-29 19:28:56 -0600

    added env covariates from Rich Fiorella and Scott Allen; added figures and linear models

--- initial_analyses/c3c4_root13C_plots.R
+++ initial_analyses/c3c4_root13C_plots.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26f8edef6d4e5306415cc7d283f0946227cb1fb6d2b8d0e0d33ad9bca5668e73
-size 6570
+oid sha256:06769efcaa9e111f95624f2eb4aa1b1a73b31ae09c0bec769548626eae222ba0
+size 10204
