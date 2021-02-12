a7a18aa97847978641ec939819d1c692e326d178
Jessica Guo <jessicaguo@github.com>
2021-02-12 09:32:28 -0700

    Hierarchical model with site-varying standard deviation as mod1b

---
+++ initial_analyses/JAGS/mod1/mod1b.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:392432be5a1a16365636102093f65fdc1f92d84ca5dbac6d8f8a8274b412add8
+size 1020
---
+++ initial_analyses/JAGS/mod1/mod1b_control.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e5f4c784db1e202e15ee62be3c27a8da3d6966fd0c70e92d6d1c0160d6740d17
+size 5029
